d8e1cda6386bbbd491a18950f8d904b30735376e
kgeographer <karlg@kgeographer.org>
2016-08-04 12:08:26 -0700

    further cleaning

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,14 @@ Bundler.require(*Rails.groups)
 
 module Authorial
   class Application < Rails::Application
-    # config.before_configuration do
-    #   env_file = Rails.root.join("config", 'local_env.yml').to_s
-    #   if File.exists?(env_file)
-    #     YAML.load_file(env_file)[Rails.env].each do |key, value|
-    #       ENV[key.to_s] = value
-    #     end
-    #   end
-    # end
+    config.before_configuration do
+      env_file = Rails.root.join("config", 'local_env.yml').to_s
+      if File.exists?(env_file)
+        YAML.load_file(env_file)[Rails.env].each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
